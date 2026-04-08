2ea62c4171bff7de973f944e3cbddac04f88f6ea
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-04-08 16:41:14 -0400

    Initial commit

---
+++ src/Auth/BigCommerceAuthProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Anibalealvarezs\BigCommerceHubDriver\Auth;
+
+use Anibalealvarezs\ApiSkeleton\Interfaces\AuthProviderInterface;
+
+class BigCommerceAuthProvider implements AuthProviderInterface
+{
+    private array $credentials = [];
+
+    public function __construct(?array $config = [])
+    {
+        $this->credentials = [
+            'access_token' => $config['bigcommerce_api_key'] ?? $_ENV['BIGCOMMERCE_API_KEY'] ?? '',
+        ];
+    }
+
+    public function getAccessToken(): string
+    {
+        return $this->credentials['access_token'] ?? '';
+    }
+
+    public function isValid(): bool
+    {
+        return !empty($this->getAccessToken());
+    }
+
+    public function isExpired(): bool
+    {
+        return false;
+    }
+
+    public function refresh(): bool
+    {
+        return false;
+    }
+
+    public function getScopes(): array
+    {
+        return [];
+    }
+
+    public function setAuthProvider(AuthProviderInterface $provider): void {}
+}
