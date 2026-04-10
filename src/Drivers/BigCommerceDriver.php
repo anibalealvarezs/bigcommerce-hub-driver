2f067628777082f86877cee1e039ff330ba5b40d
Anibal Alvarez <aalvarez@chmw.co>
2026-04-10 09:01:24 -0400

    chore: make monitoring dashboard channel labels dynamic

--- src/Drivers/BigCommerceDriver.php
+++ src/Drivers/BigCommerceDriver.php
@@ -34,6 +34,16 @@ class BigCommerceDriver implements SyncDriverInterface
         return [];
     }
 
+    /**
+     * Get the display label for the channel.
+     * 
+     * @return string
+     */
+    public static function getChannelLabel(): string
+    {
+        return 'BigCommerce';
+    }
+
     public static function getCommonConfigKey(): ?string
     {
         return null;
