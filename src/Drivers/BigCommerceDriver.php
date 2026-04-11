5bbda4711457ca072ba9830b10c86fb763a787de
Anibal Alvarez <aalvarez@chmw.co>
2026-04-11 16:23:41 -0400

    Implement mandatory getChannelIcon() method

--- src/Drivers/BigCommerceDriver.php
+++ src/Drivers/BigCommerceDriver.php
@@ -45,6 +45,16 @@ class BigCommerceDriver implements SyncDriverInterface
     }
 
     /**
+     * Get the display icon for the channel.
+     * 
+     * @return string
+     */
+    public static function getChannelIcon(): string
+    {
+        return 'BC';
+    }
+
+    /**
      * Get the routes served by this driver.
      * 
      * @return array
